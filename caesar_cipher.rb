285d6914fed3e2f6864fdd6280ecaab6d2be942d
vguti <gutierrezfloresv@gmail.com>
2016-10-17 16:46:02 +0200

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,21 @@
+def caesar_cipher(sentence,y=-3)
+  
+
+	text=sentence.downcase.split("")
+
+	text.map! do |item|
+		if item == " "
+			print item
+		else
+			x = (item.ord)+y
+			if x<97
+				x == x+26
+			elsif x>122
+				x==x-26
+			end
+			print x.chr
+		end
+	end
+end
+
+caesar_cipher("p| uhdo qdph lv grqdog gxfn")
